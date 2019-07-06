47a258c59f4baf5e07580675e776511bbd4915e1
Tomachi[ICHIGO] <lego@aol.jp>
2019-07-06 23:05:20 +0900

    Initial commit

---
+++ src/.history/js/requestParser_20190706225344.js
@@ -0,0 +1,70 @@
+(function() {
+    'use strict';
+
+    // パース正規表現定義
+    var Reg = /#([0-9]+) *(\-?[0-9]+) *(\-?[0-9]+)/;
+
+    // テキスト入力
+    // もっと効率化できると思う
+    var requestTextArea = document.getElementById('requestTextArea');
+    requestTextArea.addEventListener('input', function() {
+        var requestText = document.getElementById("requestTextArea");
+        
+        if (requestText.value == "") {
+            // からっぽ
+            requestTextArea.style.borderColor = "gray";
+            document.getElementById("requestTextAreaMessage").innerHTML = "申請内容をそのままコピペしてください。";
+            return;
+        }else if (!requestText.value.match(Reg)) {
+            // マッチしない→パースできない
+            requestTextArea.style.borderColor = "red";
+            document.getElementById("requestTextAreaMessage").innerHTML = "範囲の情報が見つかりません。";
+            return;
+        }
+
+        var reqs = requestText.value.split(/\n/);
+
+        /* 現存する入力欄をすべて消す。動作はするもののnull導入するだけなので気に入らない */
+        var points = document.getElementsByClassName("points");
+        for ( let i = 0; i < points.length; i++) {
+            points[i].innerHTML = null;
+        }
+
+        var count = 0;
+        reqs.forEach( function( value ) {
+            var result = Reg.exec(value);
+            if(result == null){
+                return;
+            }
+            var keynum = result[1]; // ex. #1
+            var X = result[2]; // ex. -640
+            var Z = result[3]; // ex. 655
+            
+            var addPoint = document.getElementById('addPoint');
+            let form = document.getElementById('chunksCounter');
+            let div = document.createElement('div');
+            div.classList.add('points');
+            let xInput = document.createElement('input');
+            let zInput = document.createElement('input');
+
+            xInput.setAttribute('type', 'number');
+            xInput.classList.add('point', 'xPoint');
+            xInput.setAttribute('placeholder', 'x');
+            xInput.value = X;
+            zInput.setAttribute('type', 'number');
+            zInput.classList.add('point', 'zPoint');
+            zInput.setAttribute('placeholder', 'z');
+            zInput.value = Z;
+            div.appendChild(xInput);
+            div.appendChild(zInput);
+            form.insertBefore(div, addPoint);
+
+            console.log("[requestParser] Added #" + keynum + " : " + X + " " + Z + " (" + chunkX + " " + chunkZ + ")");
+            count++;
+        });
+        document.getElementById("calcChunks").click();
+
+        requestTextArea.style.borderColor = "green";
+        document.getElementById("requestTextAreaMessage").innerHTML = "成功し、" + count + "個の座標データを入力しました。";
+    });
+})();
---
+++ src/js/main.js
@@ -0,0 +1,134 @@
+(function () {
+    'use strict';
+
+    // 座標入力の追加
+    var addPoint = document.getElementById('addPoint');
+    addPoint.addEventListener('click', function () {
+        let form = document.getElementById('blocksCounter');
+        let div = document.createElement('div');
+        div.classList.add('points');
+        let xInput = document.createElement('input');
+        let zInput = document.createElement('input');
+
+        xInput.setAttribute('type', 'number');
+        xInput.classList.add('point', 'xPoint');
+        xInput.setAttribute('placeholder', 'x');
+        zInput.setAttribute('type', 'number');
+        zInput.classList.add('point', 'zPoint');
+        zInput.setAttribute('placeholder', 'z');
+        div.appendChild(xInput);
+        div.appendChild(zInput);
+        form.insertBefore(div, addPoint);
+    });
+
+    // リセットボタン
+    var reset = document.getElementById('reset');
+    reset.addEventListener('click', function () {
+        var points = document.getElementsByClassName("point");
+        for (let i = 0; i < points.length; i++) {
+            points[i].value = "";
+        }
+        document.getElementById('requestTextArea').value = "";
+        document.getElementById("calcBlocks").click(); // 計算しなおす
+    });
+
+    // チャンク数計算
+    var calcBlocks = document.getElementById('calcBlocks');
+    calcBlocks.addEventListener('click', function () {
+        let result = document.getElementById('blocks');
+
+        // 座標情報取得
+        const points = getPoints('point');
+        // ブロック数計算
+        const blocks = calcBlockNumber(points);
+        result.innerText = blocks + " Blocks";
+
+        var blocksmsg = document.getElementById('blocksMessage');
+        if (blocks >= 1024) {
+            result.style.color = "red";
+            blocksmsg.innerHTML = "初期規定チャンク数(1024チャンク)以上です。新規登録の場合は「規定ブロック数を超える明確な理由」が必要です。";
+        } else if (Math.round(blocks) !== blocks) {
+            result.style.color = "orange";
+            blocksmsg.innerHTML = "チャンク数が不適切(整数でない)です。範囲が正確に指定されていない可能性があります。";
+        } else if (Math.round(blocks) == 0) {
+            result.style.color = "none";
+            blocksmsg.innerHTML = "範囲情報を入力してください。";
+        } else {
+            result.style.color = "none";
+            blocksmsg.innerHTML = "特に問題はありません。";
+        }
+    });
+
+    /**
+     * 入力された座標情報を取得
+     * @param   {string}  クラス名
+     * @return  {Object}  座標情報
+     */
+    function getPoints(className) {
+        let data = document.getElementsByClassName(className);
+        let points = {
+            x: [],
+            z: [],
+        }
+        for (let i = 0; i < data.length; i = i + 2) {
+            points.x.push(Number(data[i].value));
+            points.z.push(Number(data[i + 1].value));
+        }
+        return points;
+    }
+
+    /**
+     * 座標情報から指定範囲内のブロック数を計算
+     * @param   {Object}  座標情報
+     * @return  {number}  ブロック数
+     */
+    function calcBlockNumber(points) {
+        let size = 0;       // 面積
+        let side = 0;       // 辺の長さ
+        let blocks = 0;     // ブロック数
+
+        let x1 = 0;         // 1点目のX座標値
+        let x2 = 0;         // 2点目のX座標値
+        let z1 = 0;         // 1点目のZ座標値
+        let z2 = 0;         // 2点目のZ座標値
+
+        /* 図形の面積を計算 */
+        for (let i = 0; i < points.x.length; i++) {
+            if ((i + 1) >= points.x.length) {
+                x1 = points.x[i];
+                x2 = points.x[0];
+                z1 = points.z[i];
+                z2 = points.z[0];
+            } else {
+                x1 = points.x[i];
+                x2 = points.x[i + 1];
+                z1 = points.z[i];
+                z2 = points.z[i + 1];
+            }
+            // 外積を計算して加算
+            size += (x1 * z2) - (x2 * z1);
+        }
+        size = size / 2;
+        size = Math.abs(size);
+
+        /* 図形の辺の長さを計算 */
+        for (let i = 0; i < points.x.length; i++) {
+            if ((i + 1) >= points.x.length) {
+                side = side
+                    + Math.abs(points.x[i] - points.x[0])
+                    + Math.abs(points.z[i] - points.z[0]);
+            } else {
+                side = side
+                    + Math.abs(points.x[i] - points.x[i + 1])
+                    + Math.abs(points.z[i] - points.z[i + 1]);
+            }
+        }
+
+        /* ブロック数を計算 */
+        if (size > 0) {
+            // ブロック数 = 面積 + (辺の長さ / 2) + 1
+            blocks = size + (side / 2) + 1;
+        }
+        return blocks;
+    }
+})();
